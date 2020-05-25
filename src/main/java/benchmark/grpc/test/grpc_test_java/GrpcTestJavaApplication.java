8aecc9798535515109fd9ffe644362d58e26cd39
Oby Sumampouw <Oby Sumampouw>
2020-05-25 17:05:11 +0700

    initial project setup

---
+++ src/main/java/benchmark/grpc/test/grpc_test_java/GrpcTestJavaApplication.java
@@ -0,0 +1,13 @@
+package benchmark.grpc.test.grpc_test_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTestJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcTestJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/benchmark/grpc/test/grpc_test_java/GrpcTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package benchmark.grpc.test.grpc_test_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTestJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
