9f3d993cea340c66c5076dd027a92b0dce9f8955
Oby Sumampouw <Oby Sumampouw>
2020-05-31 20:51:45 +0700

    fix the noisy logger issue added crude way to test grpc server/client

--- src/main/java/benchmark/grpc/test/grpc_test_java/ExampleProtoServiceImpl.java
+++ src/main/java/benchmark/grpc/test/grpc_test_java/ExampleProtoServiceImpl.java
@@ -2,18 +2,27 @@ package benchmark.grpc.test.grpc_test_java;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.Random;
+
 public class ExampleProtoServiceImpl extends ExampleProtoServiceGrpc.ExampleProtoServiceImplBase{
+
+    Random random = new Random();
 
     @Override
     public void getResponse (ExampleProtobufRequest request, StreamObserver<ExampleProtobufResponse>responseObserver) {
         ExampleProtobufResponse response;
 
         if (request.getId() != null && request.getCode() != null) {
-            response = ExampleProtobufResponse.newBuilder().setStatus("200")
-                    .setResult("Foo Bar").build();
+            if (random.nextBoolean()){
+                response = ExampleProtobufResponse.newBuilder().setStatus("200")
+                        .setResult("type 1 result").build();
+            } else {
+                response = ExampleProtobufResponse.newBuilder().setStatus("200")
+                        .setResult("type 2 result").build();
+            }
         } else {
             response = ExampleProtobufResponse.newBuilder().setStatus("500")
-                    .setErrorMessage("Need Id and Code to be sent").build();
+                    .setErrorMessage("you send a request where the id or code is undefined").build();
         }
 
         responseObserver.onNext(response);
--- src/main/java/benchmark/grpc/test/grpc_test_java/GrpcClient.java
+++ src/main/java/benchmark/grpc/test/grpc_test_java/GrpcClient.java
@@ -1,57 +1,59 @@
 package benchmark.grpc.test.grpc_test_java;
 
+import ch.qos.logback.classic.Level;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.Channel;
-import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class GrpcClient {
 
-    private static void testBlocking (int retries, int maxNumThread) throws InterruptedException {
-        if (retries <= 0 || maxNumThread <= 0) {
+    private static void testBlocking (int retries, ExecutorService pool, Channel channel) throws InterruptedException {
+        if (retries <= 0) {
             throw new IllegalArgumentException();
         }
         Semaphore counter = new Semaphore(0);
-        ExecutorService pool = Executors.newFixedThreadPool(maxNumThread);
-
-        Channel channel = ManagedChannelBuilder.forAddress("localhost",
-                80).usePlaintext().build();
-
         ExampleProtoServiceGrpc.ExampleProtoServiceBlockingStub stub =
                 ExampleProtoServiceGrpc.newBlockingStub(channel);
 
         ExampleProtobufRequest request = ExampleProtobufRequest.newBuilder().setId("123").setCode("bbb").build();
+
+        final long start = System.nanoTime();
+
         for (int i = 0; i < retries; i++) {
             pool.submit(() -> {
                 ExampleProtobufResponse result =
                         stub.getResponse(request);
-                System.out.println("request " + result);
                 counter.release();
             });
         }
 
         counter.acquire(retries);
+        final long finish = System.nanoTime();
+        final long durationMs = TimeUnit.NANOSECONDS.toMillis(finish - start);
+        System.out.println("ran for " + durationMs + " ms");
     }
 
-    private static void testNonBlocking (int retries, int maxNumThread) throws InterruptedException {
-        if (retries <= 0 || maxNumThread <= 0) {
+    private static void testNonBlocking (int retries, ExecutorService pool, Channel channel) throws InterruptedException {
+        if (retries <= 0) {
             throw new IllegalArgumentException();
         }
         Semaphore counter = new Semaphore(0);
-        ExecutorService pool = Executors.newFixedThreadPool(maxNumThread);
-        Channel channel = NettyChannelBuilder.forAddress("localhost", 80).usePlaintext().build();
+
+
         ExampleProtoServiceGrpc.ExampleProtoServiceFutureStub stub =
                 ExampleProtoServiceGrpc.newFutureStub(channel);
         ExampleProtobufRequest request = ExampleProtobufRequest.newBuilder().setId("123").setCode("bbb").build();
-
+        final long start = System.nanoTime();
         for (int i = 0; i < retries; i++) {
             ListenableFuture<ExampleProtobufResponse> result =
                     stub.getResponse(request);
@@ -60,7 +62,6 @@ public class GrpcClient {
                     new FutureCallback<ExampleProtobufResponse>() {
                         @Override
                         public void onSuccess(@NullableDecl ExampleProtobufResponse result) {
-                            System.out.println("Result is " + result);
                             counter.release();
                         }
 
@@ -73,9 +74,62 @@ public class GrpcClient {
                     pool);
         }
         counter.acquire(retries);
+        final long finish = System.nanoTime();
+        final long durationMs = TimeUnit.NANOSECONDS.toMillis(finish - start);
+        System.out.println("ran for " + durationMs + " ms");
     }
 
     public static void main(String[] args) throws InterruptedException {
-        testNonBlocking(10, 5);
+        // workaround to set log level to info because somehow this application magically inject a slf4j logback
+        // and the default log level is DEBUG and netty is going to flood the logs
+        ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger)
+                LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        rootLogger.setLevel(Level.INFO);
+
+
+
+        int maxNumThread = 100;
+        ExecutorService pool = Executors.newFixedThreadPool(maxNumThread);
+
+        Channel nonBlockingChannel = NettyChannelBuilder.forAddress("localhost", 80).usePlaintext().build();
+        System.out.println("Test warm up non blocking");
+
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(5000, pool, nonBlockingChannel);
+        testNonBlocking(500, pool, nonBlockingChannel);
+        testNonBlocking(5000, pool, nonBlockingChannel);
+        testNonBlocking(500, pool, nonBlockingChannel);
+        testNonBlocking(5000, pool, nonBlockingChannel);
+
+        System.out.println("Test starts now non blocking");
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+        testNonBlocking(10000, pool, nonBlockingChannel);
+
+        Channel blockingChannel = ManagedChannelBuilder.forAddress("localhost", 80).usePlaintext().build();
+        System.out.println("Test warm up blocking");
+        testBlocking(50, pool, blockingChannel);
+        testBlocking(50, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(5000, pool, blockingChannel);
+        testBlocking(5000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+
+        System.out.println("Test starts now blocking");
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+        testBlocking(10000, pool, blockingChannel);
+
+
     }
 }
